3832620c4fe7c7221b7e1cd8807eff2033c42a78
Codngwthubaid <codngwthubaid@gmail.com>
2025-03-12 23:38:35 +0530

    Video Title and Video Topic sections are completed

--- newer/app/(main)/create/_components/VideoTopic.jsx
+++ newer/app/(main)/create/_components/VideoTopic.jsx
@@ -4,12 +4,16 @@ import React, { useState } from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Button } from '@/components/ui/button'
 import { Textarea } from "@/components/ui/textarea"
-import { SparklesIcon } from 'lucide-react'
+import { Loader, SparklesIcon } from 'lucide-react'
 import axios from 'axios'
 
 const VideoTopic = ({ onHandleInputChange }) => {
 
   const [selectedTopic, setSlectedTopic] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const [script, setScript] = useState()
+  const [selectedScriptIndex, setSelectedScriptIndex] = useState()
+
 
   const videoTopics = [
     "AI Advancements",
@@ -29,10 +33,19 @@ const VideoTopic = ({ onHandleInputChange }) => {
     "Mental Wellness"
   ];
 
-  const generateScript = async() =>{
-    console.log("generate script");
-    const result = await axios.post("/api/generate-script", {topic : selectedTopic}) 
-    console.log(result.data); 
+  const generateScript = async () => {
+    setIsLoading(true)
+    setSelectedScriptIndex(null)
+    try {
+      console.log("generate script");
+      const result = await axios.post("/api/generate-script", { topic: selectedTopic })
+      console.log(result.data?.script)
+      setScript(result.data?.script)
+    }
+    catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false)
   }
 
   return (
@@ -64,9 +77,37 @@ const VideoTopic = ({ onHandleInputChange }) => {
             <Textarea placeholder="Enter your topic ..." onChange={(e) => { onHandleInputChange("Topic", e?.target.value) }} />
           </TabsContent>
         </Tabs>
+
+        <div>
+          {script?.length > 0 && (
+            <div className='my-4 font-bold'>
+              <h2>Select the Script</h2>
+              <div className='grid grid-cols-2 gap-3'>
+                {script.map((item, index) => {
+                  return (
+                    <div key={index}>
+                      <div className='text-gray-500 font-semibold'>Script - {index + 1}</div>
+                      <div className={`line-clamp-4 border p-2 rounded-lg font-light ${selectedScriptIndex === index ? "border-2 cursor-pointer shadow-2xl text-[#fbb03b] border-[#fbb03b] hover:text-[#fbb03b]" : ""}`}
+                        onClick={() => setSelectedScriptIndex(index)} readOnly >{item.content}</div>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          )}
+        </div>
+
+
       </div>
       <div>
-        <Button className={"cursor-pointer"} onClick={generateScript}> <SparklesIcon/> Generate Script</Button>
+        {!script && <Button
+          disabled={!selectedTopic || isLoading}
+          className={"cursor-pointer"}
+          onClick={generateScript}
+        >
+          {isLoading ? <Loader className='animate-spin' /> : <SparklesIcon />}  Generate Script
+        </Button>
+        }
       </div>
     </div>
   )
--- newer/app/api/generate-script/route.jsx
+++ newer/app/api/generate-script/route.jsx
@@ -14,6 +14,6 @@ export async function  POST(request) {
     const result = await generateScript.sendMessage(PROMPT)
     const response = result?.response?.text();
     console.log(response);  
-    return NextResponse.json({response})
+    return NextResponse.json(JSON.parse(response))
 
 }
